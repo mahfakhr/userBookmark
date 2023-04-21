7d586754d5e62021a66c5d57ca5cf866499c002a
Mahnaz Fakhr <mahfakhr2009@gmail.com>
2023-04-21 16:46:42 +0330

    added strategy for make paths secure

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,6 +1,15 @@
-import {Injectable} from "@nestjs/common";
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Strategy } from 'passport-jwt';
 
-@Injectable
-export class JwtStrategy{
-
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
 }
