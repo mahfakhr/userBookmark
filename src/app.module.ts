a0f99b26ab4536b929e8e17558966f643747c710
Mahnaz Fakhr <mahfakhr2009@gmail.com>
2023-04-21 13:52:37 +0330

    init: added prj code includes auth

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AuhtModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuhtModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
+})
+export class AppModule {}
