b81c51f42e3602a79e5aa553b3e6c6b0adbfce7e
Mahnaz Fakhr <mahfakhr2009@gmail.com>
2023-04-22 17:16:04 +0330

    added user module, and statrt working with Guard

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,13 +3,17 @@ import { ExtractJwt } from 'passport-jwt';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Strategy } from 'passport-jwt';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+  constructor(config: ConfigService, prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+  validate(payload: { sub: number; email: string }) {
+    return payload;
   }
 }
